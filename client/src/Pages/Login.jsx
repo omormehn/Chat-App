73e428cb2c7a63bd0316d9431a93f33940a45db1
omormehn <nathanuseni@gmail.com>
2025-07-11 01:46:14 +0100

    clean auth ui

--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -5,21 +5,29 @@ import { useNavigate } from "react-router-dom";
 import { api } from "../utils/api.js";
 import AuthContext from "../context/AuthContext.jsx";
 import toast from "react-hot-toast";
-import { Input, Typography } from "@material-tailwind/react";
+import { Typography } from "@material-tailwind/react";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import InputComponent from "../Components/InputComponent.jsx";
+
 const Login = () => {
   const [showReg, setshowReg] = useState(false);
   const [loading, setLoading] = useState(false);
   const [errors, setErrors] = useState({});
   const [error, setError] = useState("");
+  const [isPasswordVisible, setPasswordVisible] = useState(false);
 
   const { updateUser } = useContext(AuthContext);
 
   const navigate = useNavigate();
 
+  const togglePassword = () => {
+    setPasswordVisible(!isPasswordVisible);
+  };
+
   const validateForm = (formData) => {
     const newErrors = {};
     let password = formData.get("password");
-    let confirmPassword = formData.get("confirmpassword");
+    let confirmPassword = formData.get("confirm-password");
 
     if (!formData.get("email")) {
       newErrors.email = "Email is required.";
@@ -84,7 +92,7 @@ const Login = () => {
       }
     } catch (error) {
       setError(error.response ? error.response.data.message : error.message);
-      console.log(error)
+      console.log(error);
     } finally {
       setLoading(false);
     }
@@ -97,20 +105,15 @@ const Login = () => {
   };
 
   return (
-    <div className="bg-[#343252] bg-gradient-to-r ">
+    <div className="bg-[#343252] overflow-hidden bg-gradient-to-r ">
       {/* position */}
-      <div className="flexCenter pt-10 overflow-auto px-10 h-screen ">
+      <div className="flexCenter pt-5 overflow-hidden  ">
         {/* card */}
         <div
-          className={`flex ${
+          className={`flex w-[700px] overflow-hidden min-h-screen max-w-full rounded-2xl ${
             showReg ? "flex-row-reverse" : ""
-          } bg-slate-800 card shadow-2xl`}
+          } bg-slate-800  card`}
         >
-          {/* image */}
-          <div className="flex-1 hidden sm:block">
-            <img src="third.jpg" alt="Login Visual" className="h-full w-full" />
-          </div>
-
           {/* Card sections */}
           <div className="flex-1 bg-black p-6 sm:p-10 flexCol">
             {/* Intro Texts */}
@@ -119,70 +122,71 @@ const Login = () => {
                 {!showReg ? "Login" : "Signup"}
               </h1>
             </div>
+
             <div className="pt-2">
               <form action="" onSubmit={showReg ? handleSignup : handleLogin}>
                 {/* login and reg Form */}
-                <div className="flex flex-col gap-4">
-                  <div>
-                    <Input
+                <div className="flex flex-col gap-6">
+                  <div className="bg-slate-800">
+                    <input
                       type="email"
                       name="email"
-                      label="Email"
-                      variant="standard"
                       placeholder="Email"
-                      className="bg-slate-800 w-full text-white px-2 border-0 rounded-lg  focus:ring-blue-400 "
+                      autoFocus
+                      className="auth-inputs w-full bg-black "
                     />
                     {errors.email && (
                       <p className="text-red-500 text-sm">{errors.email}</p>
                     )}
                   </div>
+
                   <div>
-                    <Input
-                      type="password"
-                      name="password"
-                      label="Password"
-                      variant="standard"
-                      placeholder="Password"
-                      className="bg-slate-800 w-full text-white px-2 border-0 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
-                    />
+                    <InputComponent
+                        name="password"
+                        placeholder="Password"
+                        isPasswordVisible={isPasswordVisible}
+                        togglePassword={togglePassword}
+                      />
                     <p className="text-red-500">{error}</p>
-                    {errors.password ? (
-                      <p className="text-red-500 text-sm">{errors.password}</p>
-                    ) : (
-                      showReg && (
-                        <Typography
-                          variant="small"
-                          color="gray"
-                          className="mt-2 flex items-center gap-1 font-normal"
-                        >
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 24 24"
-                            fill="currentColor"
-                            className="-mt-px h-4 w-4"
+                    <div>
+                      {errors.password ? (
+                        <p className="text-red-500 text-sm">
+                          {errors.password}
+                        </p>
+                      ) : (
+                        showReg && (
+                          <Typography
+                            variant="small"
+                            color="gray"
+                            className="mt-2 flex items-center gap-1 font-normal"
                           >
-                            <path
-                              fillRule="evenodd"
-                              d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm8.706-1.442c1.146-.573 2.437.463 2.126 1.706l-.709 2.836.042-.02a.75.75 0 01.67 1.34l-.04.022c-1.147.573-2.438-.463-2.127-1.706l.71-2.836-.042.02a.75.75 0 11-.671-1.34l.041-.022zM12 9a.75.75 0 100-1.5.75.75 0 000 1.5z"
-                              clipRule="evenodd"
-                            />
-                          </svg>
-                          Use at least 8 characters, one uppercase, one
-                          lowercase and one number and a special character.
-                        </Typography>
-                      )
-                    )}
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              viewBox="0 0 24 24"
+                              fill="currentColor"
+                              className="-mt-px h-4 w-4"
+                            >
+                              <path
+                                fillRule="evenodd"
+                                d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm8.706-1.442c1.146-.573 2.437.463 2.126 1.706l-.709 2.836.042-.02a.75.75 0 01.67 1.34l-.04.022c-1.147.573-2.438-.463-2.127-1.706l.71-2.836-.042.02a.75.75 0 11-.671-1.34l.041-.022zM12 9a.75.75 0 100-1.5.75.75 0 000 1.5z"
+                                clipRule="evenodd"
+                              />
+                            </svg>
+                            Use at least 8 characters, one uppercase, one
+                            lowercase and one number and a special character.
+                          </Typography>
+                        )
+                      )}
+                    </div>
                   </div>
 
                   {showReg && (
                     <div>
-                      <Input
-                        type="password"
-                        name="confirmpassword"
-                        label="Confirm Password"
+                      <InputComponent
+                        name="confirm-password"
                         placeholder="Confirm Password"
-                        variant="standard"
-                        className="bg-slate-800 w-full text-white border-0 px-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+                        isPasswordVisible={isPasswordVisible}
+                        togglePassword={togglePassword}
                       />
                       {errors.confirmPassword && (
                         <p className="text-red-500 text-sm">
@@ -192,20 +196,26 @@ const Login = () => {
                     </div>
                   )}
                   <p className="text-red-500 text-sm">{error.message}</p>
+                </div>
+
+                {/* forgot password */}
+                <div className="py-2">
                   {!showReg && (
                     <p
                       onClick={() => {
                         navigate("/resetPass");
                       }}
-                      className="text-blue-400 w-28 text-sm cursor-pointer hover:underline"
+                      className="text-blue-400  text-sm cursor-pointer hover:underline"
                     >
                       Forgot Password?
                     </p>
                   )}
                 </div>
-                <div className="flex flex-col sm:flex-row gap-2 text-white pt-2 text-slate-300 text-sm">
+
+                {/* have an account */}
+                <div className={`flex flex-col sm:flex-row gap-2 text-white text-slate-300 text-sm`}>
                   {showReg ? (
-                    <p>Have have an account? </p>
+                    <p className="">Have have an account? </p>
                   ) : (
                     <p>Don&apos;t have an account? </p>
                   )}
@@ -216,8 +226,9 @@ const Login = () => {
                     {showReg ? "Sign in" : "Sign up"}
                   </p>
                 </div>
+
                 {/* Button */}
-                <button className="primary-btn w-full mt-4 " type="submit">
+                <button className="primary-btn w-full mt-12 " type="submit">
                   {loading ? (
                     <div className="dots">
                       <div className="dot"></div>
@@ -232,7 +243,7 @@ const Login = () => {
                 </button>
               </form>
               {/* Divider */}
-              <div className="flexCenter pt-4 text-white md:gap-2">
+              <div className="flexCenter pt-14 text-white md:gap-2">
                 <div className="h-px bg-white flex-1  "></div>
                 {!showReg ? (
                   <p className="px-4 text-sm ">Or sign in with</p>
@@ -243,21 +254,12 @@ const Login = () => {
               </div>
 
               {/* Social Providers */}
-              <div className="flexCol gap-4 pt-4">
-                <div className="mini-card flexCenter gap-3py-2 bg-white rounded-lg  hover:bg-slate-100 hover:shadow-md transition">
+              <div className="flexCenter gap-4 pt-4">
+                <div className="mini-card flexCenter  rounded-lg  hover:bg-slate-100 hover:shadow-md transition">
                   <FcGoogle size={23} />
-                  <a href="" className=" text-slate-800 font-medium ">
-                    Google
-                  </a>
                 </div>
-                <div className="mini-card flexCenter gap-3py-2 bg-white rounded-lg  hover:bg-slate-100 hover:shadow-md transition">
-                  <IoLogoApple size={23} />
-                  <a
-                    href=""
-                    className=" text-slate-800 font-medium  hover:text-slate-600"
-                  >
-                    Apple
-                  </a>
+                <div className="mini-card flexCenter    rounded-lg  hover:bg-slate-100 hover:shadow-md transition">
+                  <IoLogoApple size={25} color="white" />          
                 </div>
               </div>
             </div>
